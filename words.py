d0a024d643667c7ba0c3f771349802b1c166dcd0
Mohammad Hassan <mohammadhassan@mohammads-mbp.lan>
2020-09-08 01:49:17 -0500

    python practice updated

--- words.py
+++ words.py
@@ -1,6 +1,7 @@
-def print_upper_words(array, must_start_with):
-    for i in range(len(array)):
+def print_upper_words(list, must_start_with):
+    ''' only prints words starting with must_start_with '''
+    for i in range(len(list)):
         for x in must_start_with:
-            if array[i][0] is x:
-                print(array[i])
+            if list[i][0] is x:
+                print(list[i])
 
